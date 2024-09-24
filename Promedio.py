6b9b4926bb9566fc1b7a3eea8e5835923a18a1ae
AndresNoeg <a.23308051281184@cbtis128.edu.mx>
2024-09-23 20:50:43 -0600

    Practica 4 Promedio

---
+++ Promedio.py
@@ -0,0 +1,16 @@
+print (" ")
+print ("Andres Noe Gomez Garcia Promedio ")
+print(" ")
+
+#Pide a los usuarios 3 Valores
+n1=float(input("ingrese un valor: "))
+n2=float(input("ingrese un valor: "))
+n3=float(input("ingrese un valor: "))
+
+#Se Suman los valores y se dividen entre la cantidad de valores que son en este caso 3
+suma=n1+n2+n3
+prom=suma/3
+
+#Imprime el resultado del promedio
+print("El promedio de los 3 numeros es de: ", prom)
+print(" ")
